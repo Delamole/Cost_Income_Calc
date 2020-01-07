63e99d57f754b9f0cfd1c138d4b99dd1d4b439d2
Vlad <delamole09@gmail.com>
2020-01-07 14:00:59 +0300

    Initial Commit

---
+++ my_calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  my_calc
+//
+//  Created by Владислав Бочаров on 07/01/2020.
+//  Copyright © 2020 Владислав Бочаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
